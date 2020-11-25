3ef3de523c1313cce26ce3e8883608e8b7fb23bd
Thanh <thanh@thanh.truong>
2020-11-25 13:42:38 +0700

    add filter data by attribute

--- server/roles.js
+++ server/roles.js
@@ -1,20 +1,18 @@
 const AccessControl = require("accesscontrol");
 const ac = new AccessControl();
 
-exports.roles = (function() {
-  ac.grant("basic")
-    .readOwn("profile")
-    .updateOwn("profile")
+exports.roles = (function () {
+	ac.grant("basic")
+		.readAny("profile", ["*", "!password", "!_id"])
+		.updateOwn("profile");
 
-  ac.grant("supervisor")
-    .extend("basic")
-    .readAny("profile")
+	ac.grant("supervisor").extend("basic").readAny("profile");
 
-  ac.grant("admin")
-    .extend("basic")
-    .extend("supervisor")
-    .updateAny("profile")
-    .deleteAny("profile")
+	ac.grant("admin")
+		.extend("basic")
+		.extend("supervisor")
+		.updateAny("profile")
+		.deleteAny("profile");
 
-  return ac;
+	return ac;
 })();
--- server/routes/route.js
+++ server/routes/route.js
@@ -1,17 +1,39 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const userController = require('../controllers/userController');
+const userController = require("../controllers/userController");
 
-router.post('/signup', userController.signup);
+router.post("/signup", userController.signup);
 
-router.post('/login', userController.login);
+router.post("/login", userController.login);
 
-router.get('/user/:userId', userController.allowIfLoggedin, userController.getUser);
+router.get(
+	"/user/:userId",
+	userController.allowIfLoggedin,
+	userController.grantAccess("readOwn", "profile"),
+	userController.getUser,
+	userController.filterData("readOwn", "profile"),
+);
 
-router.get('/users', userController.allowIfLoggedin, userController.grantAccess('readAny', 'profile'), userController.getUsers);
+router.get(
+	"/users",
+	userController.allowIfLoggedin,
+	userController.grantAccess("readAny", "profile"),
+	userController.getUsers,
+	userController.filterData("readOwn", "profile"),
+);
 
-router.put('/user/:userId', userController.allowIfLoggedin, userController.grantAccess('updateAny', 'profile'), userController.updateUser);
+router.put(
+	"/user/:userId",
+	userController.allowIfLoggedin,
+	userController.grantAccess("updateAny", "profile"),
+	userController.updateUser,
+);
 
-router.delete('/user/:userId', userController.allowIfLoggedin, userController.grantAccess('deleteAny', 'profile'), userController.deleteUser);
+router.delete(
+	"/user/:userId",
+	userController.allowIfLoggedin,
+	userController.grantAccess("deleteAny", "profile"),
+	userController.deleteUser,
+);
 
 module.exports = router;
